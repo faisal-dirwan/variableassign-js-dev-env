ba6abca1f7c1915eaedf3041f6bea890defe4004
Mhd Faisal Dirwan <faisaldirwan@variableassigne.com>
2017-11-27 19:00:18 -0500

    .add .eslintrc.json

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,6 +4,8 @@ import open from'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
+/*eslint-disable no-console*/
+
 const port = 3000 
 const app = express();
 const compiler = webpack(config);
@@ -16,7 +18,9 @@ app.use(require('webpack-dev-middleware')(compiler,{
 app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
-
+app.get('/favicon.ico', function(req,res) {
+    res.sendFile(path.join(__dirname, '../src/favicon.ico'));
+});
  app.listen(port, function(err){
      if (err) {
          console.log(err);
